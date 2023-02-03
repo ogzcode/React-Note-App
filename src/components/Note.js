a0905a4666eb66532acef71346467a4ca162dc1c
ogzCode <oguzguc44@gmail.com>
2023-02-03 12:34:38 +0300

    Icons added

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,21 @@
-function Note(props) {
+import { FaTrash, FaPlus } from "react-icons/fa";
+function Note({ name, date, onClick = f =>f}) {
     const handleClick = (state) => {
-        props.onClick({"name": props.name, "state": state});
+        onClick({"name": props.name, "state": state});
     };
 
     return (
         <div className='note'>
-            <h2 className='note__head'>{props.name}</h2>
+            <h2 className='note__head'>{name}</h2>
             <div className='note__bottom'>
-                <p className='note__time'>{props.date}</p>
+                <p className='note__time'>{date}</p>
                 <div className='note__btn--box'>
-                    <button className='note__btn' onClick={() => handleClick(true)}>O</button>
-                    <button className='note__btn' onClick={() => handleClick(false)}>S</button>
+                    <button className='note__btn' onClick={() => handleClick(true)}>
+                        <FaPlus/>
+                    </button>
+                    <button className='note__btn' onClick={() => handleClick(false)}>
+                        <FaTrash/>
+                    </button>
                 </div>
             </div>
         </div>
